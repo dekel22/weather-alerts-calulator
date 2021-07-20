55dc65655e9b6bce71b3257e407bbe7431286898
dekel22 <dekell8@gmail.com>
2021-07-20 16:30:12 +0300

    good

--- src/main/scala/project/services/temp.scala
+++ src/main/scala/project/services/temp.scala
@@ -9,40 +9,55 @@ import org.apache.kafka.clients.consumer.{Consumer, ConsumerConfig, KafkaConsume
 import org.apache.kafka.common.{PartitionInfo, TopicPartition}
 import org.apache.kafka.common.serialization.StringDeserializer
 import project.model.NormalizedStationCollectedData
-import org.apache.spark.sql.functions.{max}
+import org.apache.spark.sql.functions.max
+
+import java.util
 import java.util.{Collections, Properties}
 import scala.collection.JavaConverters._
+import org.apache.kafka.common.TopicPartition
+
+import scala.collection.JavaConversions.collectionAsScalaIterable
 object temp extends App {
     val props:Properties = new Properties()
-   // props.put("group.id", "test")
-    props.put("bootstrap.servers","34.67.101.108:9092")
+    props.put("group.id", "0")
+    props.put("bootstrap.servers","cnt7-naya-cdh63:9092")//34.67.101.108:9092")
     props.put("key.deserializer",
         "org.apache.kafka.common.serialization.StringDeserializer")
     props.put("value.deserializer",
         "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put("enable.auto.commit", "true")
+   props.put("enable.auto.commit", "true")
     props.put("auto.commit.interval.ms", "1000")
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
     val consumer = new KafkaConsumer(props)
-    println(consumer.listTopics())
-    val topics = List("temp")
-    consumer.subscribe(topics.asJava)
-    val records = consumer.poll(1)
+    //consumer.subscribe(util.Arrays.asList("weather_info_verified_data"))
+    val topic="weather_info_latest_raw_data"
 
 
 
 
-    val sparkSession: SparkSession = SparkSession.builder.master("local[*]").appName("example_app").getOrCreate
+
+  91043
+ // val partitions = consumer.partitionsFor(topic).map(it=>new TopicPartition(topic,it.partition)).toList.asJavaCollection
+ // consumer.assign(partitions)
+ // consumer.seekToEnd(partitions)
+  //val offsets = partitions.map(it => consumer.position(it)).toList.asJavaCollection
+  //println("s")
+
+
+  //91043
+
+  val sparkSession: SparkSession = SparkSession.builder.master("local[*]").appName("example_app").getOrCreate
     val bootstrapServer="cnt7-naya-cdh63:9092"
     val schema = Encoders.product[NormalizedStationCollectedData].schema
     val a=sparkSession
       .read
       .format("kafka")
       .option("kafka.bootstrap.servers", bootstrapServer)
-      .option("subscribe", "weather_info_verified_data").load
-      .select(col("offset")).agg(max(col("offset")))
-    println("kk")
-   //   select(col("value").cast(StringType).alias("value"))
+      .option("subscribe", "weather_info_verified_data")
+      .option("startingOffsets", """{"weather_info_verified_data":{"0":91040}}""")
+       .load
+      . select(col("value").cast(StringType).alias("value"))
 
 //    val DF = inputDF
 //      .withColumn("parsed_json", from_json(col("value"), schema))
